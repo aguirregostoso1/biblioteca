3ba6757773f55466ebb369a3f0edbf5fbda0acd0
aguirregostoso1 <aguirresantana09@gmail.com>
2024-03-07 16:34:14 -0300

    aguirre

---
+++ routes/fornecedores.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.send('e us guri');
+});
+
+/* GET users listing. */
+router.get('/produtos', function(req, res, next) {
+  res.send('e us guri');
+});
+
+
+/* GET users listing. */
+router.get('/promocoes', function(req, res, next) {
+  res.send('cr7');
+});
+
+
+/* GET users listing. */
+router.get('/listagem', function(req, res, next) {
+  res.send('neymar');
+});
+
+module.exports = router;
